745fcfe1fe228f2c24cd8c360b98634a56334632
petersonk-efsc <51683718+petersonk-efsc@users.noreply.github.com>
2021-04-23 16:34:12 -0400

    Add files via upload

--- brtest.py
+++ brtest.py
@@ -88,10 +88,11 @@ def do_it(event):
     all_lines = tmp.split('\n')
     for line in all_lines:
         whole_file.append(line + '\n')
-        tmp_str += '1' + str(whole_file) + '\n'
+    tmp_str += '1' + str(whole_file) + '\n'
     for ind in range(len(whole_file)):
         whole_file[ind] = list(whole_file[ind])
-        tmp_str += '4' + str(whole_file) + '\n'
+    tmp_str += '4' + str(whole_file) + '\n'
+    alert(str(whole_file))
     
 	remove_comments_and_strings(whole_file)
 	detabify(whole_file)
