c9b77409e04a0576708ee6a03cd240d3d128b9d7
petersonk-efsc <51683718+petersonk-efsc@users.noreply.github.com>
2021-04-23 13:56:27 -0400

    Add files via upload

---
+++ brtest.py
@@ -0,0 +1,102 @@
+def remove_comments_and_strings(whole_file):
+    """TBD.
+
+    Args:
+        TBD
+
+    Returns:
+        TBD
+    """
+    in_quote = False
+    in_single_quote = False
+    in_multi_comment = False
+    in_single_comment = False
+    prev_char = ' '
+    set_space = False
+    for line in whole_file:
+        for ind in range(len(line)):
+            set_space= False
+            if in_quote:
+                if line[ind] == '"' and prev_char != '\\':
+                    in_quote = False
+                else:
+                    set_space = True
+            elif in_single_quote:
+                if line[ind] == '\'' and prev_char != '\\':
+                    in_single_quote = False
+                else:
+                    set_space = True
+            elif in_multi_comment:
+                if line[ind] == '/' and prev_char == '*':
+                    in_multi_comment = False
+                    line[ind-1] = ' '
+                    set_space = True
+                elif line[ind] != '\n':
+                    set_space = True
+            elif in_single_comment:
+                if line[ind] == '\n':
+                    in_single_comment = False
+                else:
+                    set_space = True
+            elif line[ind] == '"':
+                in_quote = True
+            elif line[ind] == '\'':
+                in_single_quote = True
+            elif line[ind] == '*' and prev_char == '/':
+                in_multi_comment = True
+                line[ind-1] = ' '
+                set_space = True
+            elif line[ind] == '/' and prev_char == '/':
+                in_single_comment = True
+                line[ind-1] = ' '
+                set_space = True
+            prev_char = line[ind]
+            if set_space:
+                line[ind] = ' '
+
+
+def detabify(whole_file):
+    """TBD.
+
+    Args:
+        TBD
+
+    Returns:
+        TBD
+    """
+    for line in whole_file:
+        line_len = len(line)
+        ind = 0
+        while ind < line_len:
+            if line[ind] == '\t':
+                line[ind] = ' '
+                leftover = 3 - (ind % 4)
+                for _ in range(leftover):
+                    line.insert(ind, ' ')
+                ind += leftover
+            ind += 1
+
+
+from browser import document, html
+
+def do_it(event):
+    tmp = document['file1Text'].text
+    whole_file = []
+    for line in tmp.split('\n'):
+        tmp2 = list(line)
+        tmp2.append('\n')
+        whole_file.append(tmp2)
+			
+	remove_comments_and_strings(whole_file)
+	detabify(whole_file)
+
+	output_str = '<pre>'
+	for line in whole_file:
+		output_str += "".join(line)
+	output_str += '</pre>'	
+    document['results'].text = ''
+	document['results'] <= html.P(output_str)
+
+from browser import document, html
+document['run'].bind('click',do_it)
+
