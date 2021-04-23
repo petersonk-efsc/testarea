e8a0e443737dde836842323de83009215c6a7650
petersonk-efsc <51683718+petersonk-efsc@users.noreply.github.com>
2021-04-23 14:59:52 -0400

    Add files via upload

--- brtest.py
+++ brtest.py
@@ -88,11 +88,12 @@ def do_it(event):
         tmp2 = list(line)
         tmp2.append('\n')
         whole_file.append(tmp2)
-			
+	tmp_str = str(whole_file)
+    
 	remove_comments_and_strings(whole_file)
 	detabify(whole_file)
 
-	output_str = '<pre>'
+	output_str = '<pre>' + tmp_str + '\n'
 	for line in whole_file:
 		output_str += "".join(line)
 	output_str += '</pre>'	
